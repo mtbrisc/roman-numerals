05f6b7c360b1de8ffc464502875765ce0e98054a
mtbrisc <m.t.briscoe@gmail.com>
2019-05-01 18:22:40 -0600

    Point Documentation footer link to github README.md

--- client/src/modules/Footer.js
+++ client/src/modules/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className="p-5 text-center">
             <div className="container">
-                <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/mtbrisc/roman-numerals#how-to-build" target="_blank" rel="noopener noreferrer">
                     <Translate id="footer.documentation">Documentation</Translate>
                 </a>|
                 <a href="https://en.wikipedia.org/wiki/Roman_numerals" target="_blank" rel="noopener noreferrer">
